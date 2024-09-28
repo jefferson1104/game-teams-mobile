89277d391c71da924789efd62c933371f002cfc8
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-09-28 13:25:58 -0300

    feat: implementing google fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { Groups } from '@screens/Groups';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Groups } from '@screens/Groups';
 
 import theme from './src/theme';
 
 export default function App() {
+  // Hooks
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  // Renders
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
